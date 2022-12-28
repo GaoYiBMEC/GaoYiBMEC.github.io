a27005d5c00b3939328f631582c4ddc415cfb7cd
Rae <chi8243@gmail.com>
2022-12-28 13:44:44 +0800

    Create Pages

---
+++ Libs/Site.js
@@ -0,0 +1,26 @@
+﻿$(function () {
+    reportWindowSize();
+});
+
+const reportWindowSize = function () {
+    reportwindowScroll();
+};
+
+const reportwindowScroll = function () {
+    //bkWhite
+    const _header = document.getElementById('header');
+    const _headerTitle = document.getElementById('headerTitle');
+    if (_header != null && _headerTitle != null) {
+        const _headerRect = _header.getBoundingClientRect();
+        const _headerTitleRect = _headerTitle.getBoundingClientRect();
+
+        if (_headerRect.bottom < _headerTitleRect.top) {
+            $('#headerTitle').addClass('bkWhite');
+        } else {
+            $('#headerTitle').removeClass('bkWhite');
+        };
+    };
+};
+
+window.onresize = reportWindowSize;
+window.onscroll = reportwindowScroll;
---
+++ Libs/data/ImgList.js
@@ -0,0 +1,75 @@
+﻿
+window.ImgType = [
+    {
+        "ID": "RY8101",
+        "Name": "RY8101",
+        "Description": ""
+    },
+    {
+        "ID": "RY8102",
+        "Name": "RY8102",
+        "Description": ""
+    },
+    {
+        "ID": "RY8103",
+        "Name": "RY8103",
+        "Description": ""
+    },
+    {
+        "ID": "RY8104",
+        "Name": "RY8104",
+        "Description": ""
+    },
+    {
+        "ID": "RY8105",
+        "Name": "RY8105",
+        "Description": ""
+    },
+    {
+        "ID": "RY8106",
+        "Name": "RY8106",
+        "Description": ""
+    },
+    {
+        "ID": "RY8107",
+        "Name": "RY8107",
+        "Description": ""
+    },
+    {
+        "ID": "RY8108",
+        "Name": "RY8108",
+        "Description": ""
+    },
+];
+window.ImgList = [
+    {
+        "ImgType": "RY8101",
+        "ImgPath": "../Libs/Images/RY/RY8101/RY8101 (1).jpg",
+        "ImgName": "RY8101 (1).jpg"
+    },
+    {
+        "ImgType": "RY8101",
+        "ImgPath": "../Libs/Images/RY/RY8101/RY8101 (4).jpg",
+        "ImgName": "RY8101 (4).jpg"
+    },
+    {
+        "ImgType": "RY8101",
+        "ImgPath": "../Libs/Images/RY/RY8101/RY8101 (5).jpg",
+        "ImgName": "RY8101 (5).jpg"
+    },
+    {
+        "ImgType": "RY8104",
+        "ImgPath": "../Libs/Images/RY/RY8104/RY8104 (2).jpg",
+        "ImgName": "RY8104 (2).jpg"
+    },
+    {
+        "ImgType": "RY8107",
+        "ImgPath": "../Libs/Images/RY/RY8107/RY8107 (2).jpg",
+        "ImgName": "RY8107 (2).jpg"
+    },
+    {
+        "ImgType": "RY8107",
+        "ImgPath": "../Libs/Images/RY/RY8107/RY8107 (3).jpg",
+        "ImgName": "RY8107 (3).jpg"
+    }
+];
